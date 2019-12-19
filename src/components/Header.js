98687dfabf071c7ae83e3568711eade965ae0db6
Aviv <avivsegal@gmail.com>
2019-12-19 04:30:18 +0200

    add server

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,15 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import styled from "styled-components";
 import useNavigator from "../hooks/useNavigator";
-import { pathMap, pathNames } from "../configurations/routes";
-import {Brightness7 as DarkMode, Brightness4 as LightMode, GitHub} from "@material-ui/icons";
+import {pathMap, pathNames} from "../configurations/routes";
+import {Brightness4 as LightMode, Brightness7 as DarkMode, GitHub} from "@material-ui/icons";
 import IconButton from "@material-ui/core/IconButton";
-import { useDispatch, useSelector } from "react-redux";
-import { getIsDarkMode } from "../redux/ui/ui.selectors";
-import { toggleDarkTheme } from "../redux/ui/ui.actions";
+import {useDispatch, useSelector} from "react-redux";
+import {getIsDarkMode} from "../redux/ui/ui.selectors";
+import {toggleDarkTheme} from "../redux/ui/ui.actions";
 import Tooltip from "@material-ui/core/Tooltip";
-
-const StyledGitHub = styled(GitHub)`
-  fill: ${({theme}) => theme.palette.common.white};
- `;
 
 const StyledToolBar = styled(Toolbar)`
   display: flex;
@@ -27,12 +22,12 @@ const Row = styled.div`
 `;
 
 const StyledIconButton = styled(IconButton)`
-  background-color: ${({ theme, selected }) => (selected ? theme.palette.primary.dark : null)} !important;
+  background-color: ${({theme, selected}) => (selected ? theme.palette.primary.dark : null)} !important;
 `;
 
 const UpperNavigation = styled.div`
   display: block;
-  ${({ theme }) => theme.breakpoints.down("sm")} {
+  ${({theme}) => theme.breakpoints.down("sm")} {
     display: none;
   }
 `;
@@ -54,15 +49,13 @@ function Header() {
   return (
     <AppBar position="static">
       <StyledToolBar>
-        <div>
-          <Typography variant="h6" style={{ cursor: "pointer" }} onClick={() => navigate("/")}>
-            Herolo Weather Task
-          </Typography>
-        </div>
+        <Typography variant="h6" style={{cursor: "pointer"}} onClick={() => navigate("/")}>
+          Herolo Weather Task
+        </Typography>
         <Row>
           <UpperNavigation>
             {pathNames.map(pathName => {
-              const { label, Icon } = pathMap[pathName];
+              const {label, Icon} = pathMap[pathName];
               return (
                 <Tooltip key={label} title={label} aria-label={label.toLowerCase()}>
                   <StyledIconButton
@@ -71,7 +64,7 @@ function Header() {
                     value={pathName}
                     onClick={() => handleMenuItemClick(pathName)}
                   >
-                    <Icon />
+                    <Icon/>
                   </StyledIconButton>
                 </Tooltip>
               );
