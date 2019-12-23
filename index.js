23e86d9e9bac033eb86def2fda8a922cf7adbf3c
Aviv <avivsegal@gmail.com>
2019-12-23 19:58:37 +0200

    fix server

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
 let app = express();
-
 app.use(express.static(path.join(__dirname, 'build')));
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
 const port = process.env.PORT || '8080';
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+app.listen(port, () => console.log(`Running on localhost:${port}`));
+
+
